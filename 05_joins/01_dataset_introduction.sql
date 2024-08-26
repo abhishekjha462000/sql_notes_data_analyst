4ff3f0a57607dce103b40bead72ea7243b6bcbca
Abhishek jha (دانيال) <sameerjha462000>
2024-08-26 21:44:02 +0530

    @add_joins

---
+++ 05_joins/01_dataset_introduction.sql
@@ -0,0 +1,22 @@
+-- Dataset 1
+-- We have the flipkart database wherein we have 4 tables namely:
+-- category
+-- order_details
+-- orders
+-- users
+
+-- Dataset 2
+-- We have the sql_cx_live database wherein we have 7 tables namely:
+-- class
+-- groups
+-- membership
+-- person1
+-- person2
+-- students
+-- users1
+
+
+
+-- In SQL, a join is a way to combine two or more database tables based on a related column between them.
+-- Joins are used when we want to query information that is distributed across multiple tables in a database, and the information we need is not contained in a single table.
+-- By joining tables together we can create a virtual table that contains all of the information that we need for our query.
