204a2f716af48db43cf3a3a47673eb1826fde92c
Abhishek jha (دانيال) <sameerjha462000>
2024-08-26 12:41:50 +0530

    @added_sorting

--- 04_sql_grouping_sorting/01_sorting.sql
+++ 04_sql_grouping_sorting/01_sorting.sql
@@ -16,3 +16,26 @@ ORDER BY total_cameras DESC LIMIT 0,3;
 SELECT * FROM smartphones
 ORDER BY ROUND(SQRT(resolution_width * resolution_width + resolution_height * resolution_height)/screen_size, 2) DESC;
 
+
+-- Question: Find the phone with second largest battery capacity
+SELECT model, battery_capacity FROM smartphones
+ORDER BY battery_capacity DESC LIMIT 1,1;
+
+
+-- Question: Find the phone with second lowest battery capacity
+SELECT model, battery_capacity FROM smartphones
+ORDER BY battery_capacity ASC LIMIT 1,1;
+
+
+-- Question: Find the name and rating of the worst rated apple smartphone
+SELECT model, rating FROM smartphones WHERE brand_name = 'apple'
+ORDER BY rating ASC LIMIT 0,1;
+
+
+-- Question: Sort the phones alphabetically and then on the basis of rating in descending order
+SELECT model, rating FROM smartphones ORDER BY model ASC, rating DESC;
+
+
+-- Question: Sort the phones alphabetically and then on the basis of price in ascending order
+SELECT model, price FROM smartphones ORDER BY model ASC, price DESC;
+
